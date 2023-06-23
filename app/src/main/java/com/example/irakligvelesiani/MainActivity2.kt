064e880556286a96587904de59ad3a4037ca3d2a
IrakliGvelesiani <104423165+IrakliGvelesiani@users.noreply.github.com>
2023-06-23 13:11:58 +0400

    exam

--- app/src/main/java/com/example/irakligvelesiani/MainActivity2.kt
+++ app/src/main/java/com/example/irakligvelesiani/MainActivity2.kt
@@ -1,29 +1,36 @@
 package com.example.irakligvelesiani
 
-import android.widget.EditText
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
-
-    lateinit var number : TextView
-    lateinit var message : TextView
+    private lateinit var welcomeBackTextView : TextView
+    private lateinit var numberTextView : TextView
+    private lateinit var clickButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        init()
+        listeners()
+    }
 
-        number = findViewById(R.id.number)
-        message = findViewById(R.id.message)
-
-
-        val bundle = intent.extras
-        if (bundle != null){
-            number.text = "number = ${bundle.getString("number")}"
-            message.text = "message = ${bundle.getString("name")}"
-
+    private fun listeners() {
+        clickButton.setOnClickListener {
+            var number = numberTextView.text.toString().toInt()
+            number--
+            numberTextView.text = number.toString()
         }
+    }
+
+    private fun init(){
+        welcomeBackTextView = findViewById(R.id.welcomeBackTextView)
+        numberTextView = findViewById(R.id.numberTextView)
+        clickButton = findViewById(R.id.clickButton)
+
+        val email = intent.extras?.getString("EMAIL","")
+        welcomeBackTextView.text = "welcome back\n$email"
     }
 }
